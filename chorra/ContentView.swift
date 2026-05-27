b8c3a7b1771a47c4ca4188aa448be7c998a3338a
Trong Tan Nguyen <trtnguyen1@gmail.com>
2026-05-27 16:28:45 +1000

    Initial Commit

---
+++ chorra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chorra
+//
+//  Created by Tommy Nguyen on 27/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chorra/chorraApp.swift
@@ -0,0 +1,17 @@
+//
+//  chorraApp.swift
+//  chorra
+//
+//  Created by Tommy Nguyen on 27/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct chorraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
